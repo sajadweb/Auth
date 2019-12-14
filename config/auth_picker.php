4833987a7a0220eac80ab1ccac67b4494800e809
sajad web <sajadweb7@gmail.com>
2019-12-14 21:28:51 +0330

    fixed bug in auth

--- config/auth_picker.php
+++ config/auth_picker.php
@@ -1,11 +1,6 @@
 <?php
 
-return [
-    "name"=> "BUNDLE",
-    "client_key" => "12345678-1234-1234-1234-4f7a75aa80f8",
-    "server_key" => "83v65723-1234-1234-1234-4f7a75aa80b1",
-    "JWT"=>  "RS256",
-    "private_key" => <<<EOD
+$private=<<<EOD
 -----BEGIN RSA PRIVATE KEY-----
 MIIBOQIBAAJAdN9r3p5QQ1XMU9y1C/TDGwcn71v4uud9eKUVCIvUvYv10SnKGfbZ
 7RRQuorS0xrVBmAxR5PDOba1+8AApGHd2QIDAQABAkBceTdxstQRIz8ED/sUlbsW
@@ -15,12 +10,21 @@ AiEAwjq4GH3JSy8ITuHo9uTnYILQ+z1QtCNAlY5Lt4FxIEMCIQCaCqxwWgi5IZqg
 PwEyKZZ0b/MCIQCOG8Kxc54QjLvGQGOdSA1+VMtj4uazhPBhis6YDeQSmQIgLPYg
 uPIBEHZ9Qd7nakad5qTj3Bkw89DeW7bTqscwA3I=
 -----END RSA PRIVATE KEY-----
-EOD,
-    "public_key" => <<<EOD
+EOD;
+
+$public=<<<EOD
 -----BEGIN PUBLIC KEY-----
 MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAdN9r3p5QQ1XMU9y1C/TDGwcn71v4uud9
 eKUVCIvUvYv10SnKGfbZ7RRQuorS0xrVBmAxR5PDOba1+8AApGHd2QIDAQAB
 -----END PUBLIC KEY-----
-EOD,
+EOD;
+
+return [
+    "name"=> "BUNDLE",
+    "client_key" => "12345678-1234-1234-1234-4f7a75aa80f8",
+    "server_key" => "83v65723-1234-1234-1234-4f7a75aa80b1",
+    "JWT"=>  "RS256",
+    "private_key" => $private,
+    "public_key" => $public
 ];
 
